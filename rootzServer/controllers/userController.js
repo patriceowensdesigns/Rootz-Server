ee681539d3d094096c4e8c4fa691fa45524d9b47
patriceowensdesigns <patriceowensdesigns@gmail.com>
2021-04-06 21:50:34 -0400

    added sessionToken, .evn file updated gitignore

--- rootzServer/controllers/userController.js
+++ rootzServer/controllers/userController.js
@@ -1,5 +1,7 @@
 const router = require("express").Router();
 const User = require("../db").import("../models/user");
+const jwt = require("jsonwebtoken");
+
 
 router.post('/create', function (req, res) {
     User.create({
@@ -8,8 +10,11 @@ router.post('/create', function (req, res) {
     })
         .then(
             function createSuccess(user) {
+                let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
                 res.json({
-                    user: user
+                    user: user,
+                    message: 'User successfully created!',
+                    sessionToken: token
                 });
             }
         )
@@ -25,8 +30,12 @@ router.post('/login', function(req, res) {
     .then(
         function loginSuccess(user){
         if (user) {
+            let token = jwt.sign({id: user.id}, process.env.JWT_SECRET, {expiresIn: 60 * 60 * 24});
+
             res.status(200).json({
-                user: user
+                user: user,
+                message: "User successfully logged in!",
+                sessionToken: token
             })
         } else{
             res.status(500).json({error: 'User does not exist.'})
